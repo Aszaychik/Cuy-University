ac219ba7a241061b9356a0026e95a881ee9348dc
AsZaychik <aszaychik@gmail.com>
2022-10-16 22:39:53 +0700

    Semester 3 Sesi01-04

--- Semester3/Sesi01-04/index.js
+++ Semester3/Sesi01-04/index.js
@@ -3,11 +3,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require(`body-parser`);
+const db = require("./connection");
+const response = require("./response");
 
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1 align='center'>Hello World!</h1>");
+  db.query("SELECT * FROM characters", (error, result) => {
+    response(200, result, "get all data from characters", res);
+  });
+});
+
+app.get("/school", (req, res) => {
+  const sql = `SELECT Name FROM characters WHERE School = ${req.query.School}`;
+  db.query(sql, (error, result) => {
+    response(200, result, "Find Characters By School", res);
+  });
+  console.log(`Find School : ${req.query.School}`);
 });
 
 app.get("/author", (req, res) => {
